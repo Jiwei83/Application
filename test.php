d4e72bb0ad6a3f75601bbcb12e84712e35418c7a
Jiwei83 <kricma83@gmail.com>
2017-07-19 04:46:37 -0700

    email unique

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $host = '127.0.0.1';
 $user = 'root';
 $pass = 'root';
 $db   = 'test';
-//createTable($host, $user, $pass, $db);
+createTable($host, $user, $pass, $db);
 //connect2Databse($host, $user, $pass, $db);
 $data = getFile('123.csv');
 insertData($data, $host, $user, $pass, $db);
@@ -30,6 +30,7 @@ function connect2Databse($host, $user, $pass, $db) {
     	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     	PDO::ATTR_EMULATE_PREPARES   => false,
+    	PDO::ATTR_ERRMODE            => PDO::ERRMODE_WARNING,
 	];
 	$pdo = new PDO($dsn, $user, $pass, $opt);
 	return $pdo;
@@ -37,7 +38,7 @@ function connect2Databse($host, $user, $pass, $db) {
 
 function createTable($host, $user, $pass, $db) {
 	$pdo = connect2Databse($host, $user, $pass, $db);
-	$statement = "CREATE TABLE IF NOT EXISTS USERS (ID INT(11) AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(50) NOT NULL, SURNAME VARCHAR(50) NOT NULL, EMAIL VARCHAR(100) NOT NULL);";
+	$statement = "CREATE TABLE IF NOT EXISTS USERS (ID INT(11) AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(50) NOT NULL, SURNAME VARCHAR(50) NOT NULL, EMAIL VARCHAR(100) NOT NULL UNIQUE);";
 	$table = $pdo->exec($statement);
 
 	if ($table !== false) {
@@ -50,8 +51,7 @@ function createTable($host, $user, $pass, $db) {
 
 function insertData($data, $host, $user, $pass, $db) {
 	$pdo = connect2Databse($host, $user, $pass, $db);
-	try {
-     foreach($data as $record) {
+ 	foreach($data as $record) {
 		$sql = "INSERT INTO USERS (NAME, SURNAME, EMAIL) VALUES(:name, :surname, :email)";
 		if (filter_var(trim($record[2]), FILTER_VALIDATE_EMAIL)) {
 			$statement = $pdo->prepare($sql);
@@ -60,19 +60,18 @@ function insertData($data, $host, $user, $pass, $db) {
 			'surname' => ucfirst(trim($record[1])),
 			'email'   => strtolower(trim($record[2]))
 			));
-			echo "New records inserted successfully \r\n";
+			if ($statement->errorCode() == 0) {
+				echo "New records inserted successfully \r\n";
+			}
+			else {
+				echo $statement->errorInfo()[2].'\r\n';
+				continue;
+			}
+			
 		}
 		else {
 			echo "$record[2] Email Invalid!! \r\n";
 		}
-	}
-	} 
-	catch (PDOException $e) {
-	    if ($e->getCode() == 1062) {
-	        // Take some action if there is a key constraint violation, i.e. duplicate name
-	    } else {
-	        throw $e;
-	    }
 	}
 }
 	
